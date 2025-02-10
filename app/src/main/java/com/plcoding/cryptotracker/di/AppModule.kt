99c406cbfc0bc9d4702628442b8a5a08112f377d
khorevigor <dostup.96@gmail.com>
2025-02-10 22:02:55 +0300

    Remove proxy from http engine builder

--- app/src/main/java/com/plcoding/cryptotracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/cryptotracker/di/AppModule.kt
@@ -4,14 +4,18 @@ import com.plcoding.cryptotracker.core.data.networking.HttpClientFactory
 import com.plcoding.cryptotracker.crypto.data.networking.RemoteCoinDataSource
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
+import io.ktor.client.engine.ProxyBuilder
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.http
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val appModule = module {
-    single { HttpClientFactory.create(CIO.create()) }
+    single { HttpClientFactory.create(CIO.create {
+//        proxy = ProxyBuilder.http("http://172.22.9.200:8080")
+    }) }
     singleOf(::RemoteCoinDataSource).bind<CoinDataSource>()
 
     viewModelOf(::CoinListViewModel)
